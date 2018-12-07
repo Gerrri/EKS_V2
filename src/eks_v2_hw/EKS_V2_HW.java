512c68c86b85453bce0c977f8f506844b988d74f
Gerrri <dustin-a@gmx.de>
2018-12-07 11:34:44 +0100

    init

---
+++ src/eks_v2_hw/EKS_V2_HW.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eks_v2_hw;
+
+/**
+ *
+ * @author Dustin
+ */
+public class EKS_V2_HW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/eks_v2_hw/entities/Reise.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eks_v2_hw.entities;
+
+/**
+ *
+ * @author Dustin
+ */
+public class Reise {
+    
+}
