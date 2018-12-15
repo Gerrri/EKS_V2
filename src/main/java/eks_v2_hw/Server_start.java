989362ed8110ebc71dfa6e2bfe5daf5fe2a1b614
Dustin Asbach aka Gerrri <dustin-a@gmx.de>
2018-12-15 13:48:50 +0100

    umstieg auf maven ...

---
+++ src/main/java/eks_v2_hw/Server_start.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eks_v2_hw;
+
+import com.sun.net.httpserver.HttpServer;
+import eks_v2_hw.server.res.VeranstalterService;
+import java.net.URI;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+
+/**
+ *
+ * @author Dustin
+ */
+public class Server_start {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception{
+        URI endpoint = new URI("http://localhost:55554/reiseservice");
+        ResourceConfig rc = new ResourceConfig(VeranstalterService.class);
+        HttpServer server = JdkHttpServerFactory.createHttpServer(endpoint, rc);
+        System.out.println("Server wurde gestarten :)");
+    }
+    
+}
